3f5971a5ffea6e6681f84a2e62b6952bfe4441d9
jefaokpta <jefaokpta@hotmail.com>
2023-01-30 12:30:10 -0300

    fazendo ligacoes ok

--- js/ua.js
+++ js/ua.js
@@ -4,6 +4,9 @@ const peer = secureData.peer;
 const password = secureData.password;
 let callSession;
 let sipStack;
+// todo: descarregar audio de ring para botao play nao ativar o audio
+const ringAudio = new Audio();
+const phone = document.getElementById('phone');
 
 const eventsListener = (event) => {
     switch (event.type) {
@@ -18,18 +21,30 @@ const eventsListener = (event) => {
             break;
         case 'terminated':
             console.log(':::::::::::::: DESLIGADO')
+            ringAudio.pause();
+            ringAudio.src = '';
             break;
         case 'connecting':
             console.log(':::::::::::::: CONECTANDO AO SERVER')
             break;
         case 'connected':
             console.log(':::::::::::::: ATENDIDO OU REGISTRADO')
+            ringAudio.pause();
             break;
         case 'i_new_message':
             console.log('::::::::::::::::: EVENTO NOVA MENSAGEM: ');
             break;
         case 'i_new_call':
             console.log('::::::::::::::::: EVENTO LIGACAO DE ENTRADA: ');
+            break;
+        case 'i_ao_request':
+            console.log('I_AO_REQUEST', event);
+            if(event.description === 'Ringing') {
+                ringAudio.play();
+                ringAudio.loop = true;
+            } else{
+                ringAudio.pause();
+            }
             break;
         default:
             console.log('::::::::::::::::::::: EVENTO DESCONHECIDO = ' + event.type);
@@ -38,19 +53,33 @@ const eventsListener = (event) => {
 }
 
 function doCall() {
+    ringAudio.src = '/assets/calling.mp3';
     callSession = sipStack.newSession('call-audio', {
         video_local: document.getElementById('video-local'),
         video_remote: document.getElementById('video-remote'),
         audio_remote: document.getElementById('audio-remote'),
         events_listener: { events: '*', listener: eventsListener } // optional: '*' means all events
     });
-    callSession.call('1001001');
+
+    // const telNumber = '1132931515'
+    // const telNumber = '08007627777'
+    // const telNumber = '1142325502'
+    // const telNumber = '11938065778'
+
+    callSession.call(phone.value);
+
 }
 
 function hangup() {
     callSession.hangup();
 }
 
+function sendDTMF9() {
+    callSession.dtmf(9);
+}
+function sendDTMF2() {
+    callSession.dtmf(2);
+}
 
 function createSipStack() {
     sipStack = new SIPml.Stack({
@@ -67,11 +96,11 @@ function createSipStack() {
             { name: 'User-Agent', value: 'WEBPHONE JPBX' },
             { name: 'Organization', value: 'JPBX' }
         ]
-    }
-    );
+    });
 }
 
-var registerBtn = document.getElementById('register');
+const registerBtn = document.getElementById('register');
+
 registerBtn.addEventListener('click', function () {
     const registerSession = sipStack.newSession('register', {
         events_listener: { events: '*', listener: eventsListener }// optional: '*' means all events
@@ -79,7 +108,10 @@ registerBtn.addEventListener('click', function () {
     registerSession.register();
 });
 
+function startSipStack() {
+    sipStack.start();
+}
+
 window.onload = function () {
     this.createSipStack();
-    sipStack.start();
 };
