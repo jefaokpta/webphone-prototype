50f60e53844254eff137d8841f0bf512365b781d
jefaokpta <jefaokpta@hotmail.com>
2023-01-31 13:49:47 -0300

    acho q ta completo

--- js/ua-receber.js
+++ js/ua-receber.js
@@ -2,9 +2,9 @@ const domain = secureData.domain;
 const peer = secureData.peer;
 const password = secureData.password;
 let callSession;
+const callAudio = new Audio();
 
-const eventsListener = (event) => {
-
+const mainEventsListener = (event) => {
     switch (event.type) {
         case 'starting':
             console.log(':::::::::::::: INICIANDO SIP STACK')
@@ -13,44 +13,58 @@ const eventsListener = (event) => {
             console.log(':::::::::::::: SIP STACK COMPLETO')
             sipStack.newSession('register', {}).register()
             break;
-        case 'terminating':
-            console.log(':::::::::::::: DESLIGANDO')
-            break;
-        case 'terminated':
-            console.log(':::::::::::::: DESLIGADO')
-            ringAudio.pause();
-            ringAudio.src = '';
-            break;
-        case 'connecting':
-            console.log(':::::::::::::: CONECTANDO AO SERVER')
-            break;
-        case 'connected':
-            console.log(':::::::::::::: ATENDIDO OU REGISTRADO')
-            ringAudio.pause();
-            break;
-        case 'i_new_message':
-            console.log('::::::::::::::::: EVENTO NOVA MENSAGEM: ');
-            break;
         case 'i_new_call':
-            console.log(event)
-            console.log('::::::::::::::::: LIGACAO DE ENTRADA de : ', event.newSession.getRemoteFriendlyName());
+            event.newSession.setConfiguration({
+                events_listener: {events: '*', listener: incomingCallEventsListener},
+            })
+            console.log('::::::::::::::::: LIGACAO DE ENTRADA DE : ', event.newSession.getRemoteFriendlyName());
+            callAudio.src = 'assets/ringing.mp3';
+            playAudio()
             callSession = event.newSession;
-            break;
-        case 'i_ao_request':
-            console.log('I_AO_REQUEST', event);
-            if (event.description === 'Ringing') {
-                ringAudio.play();
-                ringAudio.loop = true;
-            } else {
-                ringAudio.pause();
-            }
             break;
         default:
             console.log('::::::::::::::::::::: EVENTO DESCONHECIDO = ', event.type);
             break;
     }
 }
-
+const outboundCallEventsListener = (event) => {
+    switch (event.type) {
+        case 'i_ao_request':
+            console.log('I_AO_REQUEST', event);
+            if (event.description === 'Ringing') {
+                playAudio()
+            }
+            break;
+        case 'terminated':
+            pauseAudio()
+            break;
+        case 'terminating':
+            pauseAudio()
+            break;
+        case 'connected':
+            pauseAudio()
+            break;
+        default:
+            console.log('>>>>>>>>>>>>>>> OUT CALL EVENT DESCONHECIDO = ', event.type);
+            break;
+    }
+}
+const incomingCallEventsListener = (event) => {
+    switch (event.type) {
+        case 'terminated':
+            pauseAudio()
+            break;
+        case 'terminating':
+            pauseAudio()
+            break;
+        case 'connected':
+            pauseAudio()
+            break;
+        default:
+            console.log('<<<<<<<<<<<<<<< IN CALL EVENT DESCONHECIDO = ', event.type);
+            break;
+    }
+}
 
 const sipStack = new SIPml.Stack({
     realm: domain, // mandatory: domain name
@@ -62,7 +76,7 @@ const sipStack = new SIPml.Stack({
     //outbound_proxy_url: 'udp://example.org:5060', // optional
     ice_servers: [{url: 'stun:stun.l.google.com:19302'}],
     enable_rtcweb_breaker: false, // optional
-    events_listener: {events: '*', listener: eventsListener}, // optional: '*' means all events
+    events_listener: {events: '*', listener: mainEventsListener}, // optional: '*' means all events
     sip_headers: [ // optional
         {name: 'User-Agent', value: 'WEBPHONE JPBX'},
         {name: 'Organization', value: 'JPBX'}
@@ -74,9 +88,13 @@ window.onload = () => {
 }
 
 const answerBtn = document.getElementById('answer');
+const rejectBtn = document.getElementById('reject');
 const hangupBtn = document.getElementById('hangup');
 const dialBtn = document.getElementById('dial');
 
+rejectBtn.addEventListener('click', () => {
+    callSession.reject();
+})
 answerBtn.addEventListener('click', () => {
     callSession.accept();
 })
@@ -84,12 +102,23 @@ hangupBtn.addEventListener('click', () => {
     callSession.hangup();
 })
 dialBtn.addEventListener('click', () => {
+    callAudio.src = 'assets/calling.mp3';
     const number = document.getElementById('phoneInput').value;
     callSession = sipStack.newSession('call-audio', {
         audio_remote: document.getElementById('audio-remote'),
+        events_listener: {events: '*', listener: outboundCallEventsListener}
     });
     callSession.call(number);
 })
+
+function playAudio() {
+    callAudio.play();
+    callAudio.loop = true;
+}
+function pauseAudio() {
+    callAudio.pause();
+    callAudio.src = '';
+}
 
 function sendDtmf(dtmf) {
     callSession.dtmf(dtmf);
