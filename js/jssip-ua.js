6072d5ab93b2acc93c89eb5eb705897c2a724c3b
jefaokpta <jefaokpta@hotmail.com>
2023-10-27 16:59:21 -0300

    audio melhor

--- js/jssip-ua.js
+++ js/jssip-ua.js
@@ -37,7 +37,7 @@ const callEventHandlers = {
 function audioListener(session){
     session.connection.addEventListener('addstream', e => {
         console.log('remote audio stream added');
-        const audio = document.createElement('audio');
+        const audio = new Audio()
         audio.srcObject = e.stream;
         audio.play();
     });
