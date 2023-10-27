38efc562fd6ac929c3095fe9cb85d27684599364
jefaokpta <jefaokpta@hotmail.com>
2023-10-27 15:37:24 -0300

    jssip fazendo ligacoes ok!

--- js/jssip-ua.js
+++ js/jssip-ua.js
@@ -10,48 +10,93 @@ const configuration = {
     sockets  : [ socket ],
     uri      : `sip:${peer}@` + domain,
     password : password,
+    register : false,
 };
 
 const ua = new JsSIP.UA(configuration);
-var session;
+let session = null;
 
+uaEventHandling();
 ua.start();
 
-// Register callbacks to desired call events
-const eventHandlers = {
+const callEventHandlers = {
     'progress': function(e) {
         console.log('call is in progress');
     },
     'failed': function(e) {
-        console.log(e)
-        console.log('call failed with cause: '+ e);
+        console.log(`call failed originator: ${e.originator} with cause: ${e.cause}`);
     },
     'ended': function(e) {
-        console.log(e)
-        console.log('call ended with cause: '+ e);
+        console.log(`call ended originator: ${e.originator} with cause: ${e.cause}`);
     },
     'confirmed': function(e) {
-        console.log('call confirmed');
+        console.log('call atendida');
     }
 };
 
-const options = {
-    'eventHandlers'    : eventHandlers,
+function audioListener(session){
+    session.connection.addEventListener('addstream', e => {
+        console.log('remote audio stream added');
+        const audio = document.createElement('audio');
+        audio.srcObject = e.stream;
+        audio.play();
+    });
+}
+
+const callOptions = {
+    'eventHandlers'    : callEventHandlers,
     'mediaConstraints' : { 'audio': true, 'video': false },
+    'pcConfig'         : {
+        'rtcpMuxPolicy': 'require',
+        'iceServers'   : [
+            { 'urls': 'stun:stun.l.google.com:19302' }
+        ]
+    }
 };
+
+function uaEventHandling() {
+    //events of UA class with their callbacks
+
+    ua.on('registered', function (e) {
+        console.trace("registered", e);
+    });
+
+    ua.on('unregistered', e => {
+        console.trace("ua has been unregistered periodic registeration fails or ua.unregister()", e);
+    });
+    ua.on('registrationFailed', e => {
+        console.trace("register failed", e);
+    });
+    ua.on('connected', e => {
+        console.trace("connected to websocket");
+    });
+
+    ua.on('disconnected', e => {
+        console.trace("disconnected");
+        ua.stop();
+    });
+    ua.on('newRTCSession', e => {
+        session = e.session;
+        if (e.originator === 'local') {
+            console.trace(e.request + ' start outgoing session');
+            audioListener(session);
+        }
+        else {
+            console.trace(e.request + ' start incoming session');
+            //e.session.answer(options);
+        }
+    });
+
+}
 
 function dial() {
     const number = document.getElementById('number').value;
-    session = ua.call(`sip:${number}@${domain}`, options);
-    if (session) {
-        session.connection.addEventListener('addstream', (e) => {
-            const audio = document.createElement('audio');
-            audio.srcObject = e.stream;
-            audio.play();
-        });
-    }
+    session = ua.call(`sip:${number}@${domain}`, callOptions);
 }
+
 function hangup() {
     session.terminate();
 }
+
+
 
